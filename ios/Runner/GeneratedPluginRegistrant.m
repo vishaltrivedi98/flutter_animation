e2a4ab45b69747fd5ba380825a90f239a15da334
vishaltrivedi98 <70379387+vishaltrivedi98@users.noreply.github.com>
2020-11-07 18:46:16 +0530

    Add files via upload

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,19 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<documents_picker/DocumentsPickerPlugin.h>)
+#import <documents_picker/DocumentsPickerPlugin.h>
+#else
+@import documents_picker;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [DocumentsPickerPlugin registerWithRegistrar:[registry registrarForPlugin:@"DocumentsPickerPlugin"]];
+}
+
+@end
